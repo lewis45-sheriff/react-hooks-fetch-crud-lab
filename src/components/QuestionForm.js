c126dcbdfc2a46543d51d8c4396c89cad489f876
lewis <lewiskipkemoi53@gmail.com>
2024-08-09 10:57:14 +0300

    all

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,35 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+
+    // Preparing the data to be sent to the server
+    const dataToSend = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+
+    // Sending the POST request to the server
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // You can add further actions like resetting the form or showing a success message
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   return (
