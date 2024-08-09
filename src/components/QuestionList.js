95b09fe114d39b7bf1591d47b0c31bac6e04725a
lewis <lewiskipkemoi53@gmail.com>
2024-08-09 09:56:21 +0300

    all

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("There was a problem with the fetch operation:", error));
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>{question.prompt}</li>
+        ))}
+      </ul>
     </section>
   );
 }
